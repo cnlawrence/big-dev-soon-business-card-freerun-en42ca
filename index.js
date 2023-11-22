1f0b7f4ea8574b888e2847b4c386a0a250b5300b
cnlawrence1183 <cnlawrence1183@gmail.com>
2023-11-22 11:20:55 -0600

    Adding Tailwindcss to project

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* This is an example JavaScript file, feel free to remove/edit it anytime */
 console.log(
-  "%cProject by BigDevSoon",
+  "%cProject by cnlawrence",
   'font-size: 40px; font-weight: bold; color: #8A2BE2; font-family: "Comic Sans MS", cursive, sans-serif;'
 );
-console.log("Check out more projects at https://bigdevsoon.me");
+console.log("Check out more projects at https://github.com/cnlawrence");
